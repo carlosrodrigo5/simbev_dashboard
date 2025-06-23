da7f002b0feaff5ef65b3becd7a7cfc94d455af6
Carlos Canales <carloscanales@MacBook-Pro-von-admin.local>
2025-06-23 15:00:59 +0200

    code linting

--- dashboard.py
+++ dashboard.py
@@ -35,34 +35,12 @@ hv.renderer("bokeh").theme = "light_minimal"
 simbev_results = pd.read_parquet(
     "data/simbev_public_installed_charging_power_and_yearly_energy.parquet"
 )
-ss_analysis = pd.read_parquet(
-    "data/simbev_sensitivity_analysis.parquet"
-)
+ss_analysis = pd.read_parquet("data/simbev_sensitivity_analysis.parquet")
 
 
 # data extraction
 def create_scenario_graphs():
-    source_data = simbev_results.copy()
-    # Widgets
-    scenario_selector = pn.widgets.Select(
-        name="Szenario \n (nur für Tabelle)",
-        options=["Alle"] + list(source_data["scenario"].unique()),
-        height=80,
-        sizing_mode="scale_width",
-    )
-    unit_selector = pn.widgets.Select(
-        name="Unit",
-        options=list(source_data["unit"].unique())[::-1],
-        height=80,
-        sizing_mode="scale_width",
-    )
-    luc_selector = pn.widgets.Select(
-        name="LUC",
-        options=list(source_data["LUC"].unique()),
-        height=80,
-        sizing_mode="scale_width",
-    )
-
+    # --------------- Functions -----------
     def filter_data_table(scenario, luc, unit):
         data = source_data.copy()
         data["year"] = data["year"].astype(int).astype(str)
@@ -91,39 +69,80 @@ def create_scenario_graphs():
         #     yformatter='%.0f'
         # ).opts(legend_opts={"click_policy": "hide"})
 
-    # df for table
-    df_table = pn.rx(filter_data_table)(
-        scenario=scenario_selector, luc=luc_selector, unit=unit_selector
-    )
-    # df_plot = pn.rx(filter_data_plot)(luc=luc_selector, unit=unit_selector)
-    df_plot = hvplot.bind(
-        filter_data_plot, luc=luc_selector, unit=unit_selector
-    ).interactive()
-
-    def filtered_file(scenario, luc, unit):
+    def get_filtered_file(scenario, luc, unit):
         df = filter_data_table(scenario, luc, unit)
         sio = StringIO()
         df.to_csv(sio)
         sio.seek(0)
         return sio
 
-    fd = pn.widgets.FileDownload(
-        callback=pn.bind(
-            filtered_file,
-            scenario=scenario_selector,
-            luc=luc_selector,
-            unit=unit_selector,
-        ),
-        filename="data.csv",
+    def modify_legend(plot, element):
+        if hasattr(plot.state, "legend"):
+            for legend in plot.state.legend:
+                legend.title = "Szenario"  # Set legend title
+                legend.title_text_font_size = "12pt"  # Customize title size
+                legend.title_text_font_style = "bold"  # Set title style
+                legend.border_line_color = "black"  # Add a border
+                legend.background_fill_color = "white"  # Change background
+                legend.title_text_align = "center"
+                legend.label_standoff = 10  # Adjust spacing to simulate centering
+                legend.spacing = 5  # Control spacing between legend items
+                legend.margin = 0  # Adjust legend padding
+                legend.location = (-80, 0)  # Adjust legend position
+
+    def sort_legend(plot, element):
+        plot.handles["plot"].legend[0].items.reverse()
+
+    # update luc_selector options based on unit selection
+    def update_luc_selector(event):
+        if event.new == "MW":
+            luc_selector.options = list(
+                source_data[source_data["unit"] == "MW"]["LUC"].unique()
+            )
+        elif event.new == "GWh/year":
+            luc_selector.options = list(
+                source_data[source_data["unit"] == "GWh/year"]["LUC"].unique()
+            )
+        else:
+            luc_selector.options = list(source_data["LUC"].unique())
+
+    # --------------- Read Data  -----------
+
+    source_data = simbev_results.copy()
+
+    # -------------- Create Widgets --------------
+    scenario_selector = pn.widgets.Select(
+        name="Szenario \n (nur für Tabelle)",
+        options=["Alle"] + list(source_data["scenario"].unique()),
+        height=80,
+        sizing_mode="scale_width",
+    )
+    unit_selector = pn.widgets.Select(
+        name="Unit",
+        options=list(source_data["unit"].unique())[::-1],
+        height=80,
+        sizing_mode="scale_width",
+    )
+    luc_selector = pn.widgets.Select(
+        name="LUC",
+        options=list(source_data["LUC"].unique()),
+        height=80,
+        sizing_mode="scale_width",
     )
 
-    hover = HoverTool(
-        tooltips=[
-            ("Szenario", "@scenario"),
-            ("Jahr", "@year"),
-            ("Werte", "@value{0.0,0}"),
-        ]
+    widgets = pn.Row(
+        scenario_selector, unit_selector, luc_selector, sizing_mode="scale_width"
     )
+
+    # --------------- Data Filtering --------------
+    df_table = pn.rx(filter_data_table)(
+        scenario=scenario_selector, luc=luc_selector, unit=unit_selector
+    )
+    df_plot = hvplot.bind(
+        filter_data_plot, luc=luc_selector, unit=unit_selector
+    ).interactive()
+
+    # --------------- Create Plots --------------
 
     # fig = df_plot.hvplot.scatter(
     #     x='year', y='value', by='scenario',
@@ -136,6 +155,14 @@ def create_scenario_graphs():
     # ).opts(legend_opts={"click_policy": "hide"})
 
     # legend as a single row under the graph
+
+    hover = HoverTool(
+        tooltips=[
+            ("Szenario", "@scenario"),
+            ("Jahr", "@year"),
+            ("Werte", "@value{0.0,0}"),
+        ]
+    )
 
     fig_line = df_plot.hvplot.line(
         x="year",
@@ -194,48 +221,25 @@ def create_scenario_graphs():
     # line graph that join the scatter points
     fig = fig_line * fig_scatter
 
-    def modify_legend(plot, element):
-        if hasattr(plot.state, "legend"):
-            for legend in plot.state.legend:
-                legend.title = "Szenario"  # Set legend title
-                legend.title_text_font_size = "12pt"  # Customize title size
-                legend.title_text_font_style = "bold"  # Set title style
-                legend.border_line_color = "black"  # Add a border
-                legend.background_fill_color = "white"  # Change background
-                legend.title_text_align = "center"
-                legend.label_standoff = 10  # Adjust spacing to simulate centering
-                legend.spacing = 5  # Control spacing between legend items
-                legend.margin = 0  # Adjust legend padding
-                legend.location = (-80, 0)  # Adjust legend position
-
-    def sort_legend(plot, element):
-        plot.handles["plot"].legend[0].items.reverse()
-
     fig = fig.opts(hooks=[modify_legend, sort_legend], show_grid=True, axiswise=True)
 
-    # creating components
-    # plot = pn.pane.HoloViews(fig, sizing_mode='stretch_both', name='plot')
+    # ------------------- Create Table --------------
     table = pn.widgets.Tabulator(df_table, name="table", sizing_mode="scale_both")
 
-    # update luc_selector options based on unit selection
-    def update_luc_selector(event):
-        if event.new == "MW":
-            luc_selector.options = list(
-                source_data[source_data["unit"] == "MW"]["LUC"].unique()
-            )
-        elif event.new == "GWh/year":
-            luc_selector.options = list(
-                source_data[source_data["unit"] == "GWh/year"]["LUC"].unique()
-            )
-        else:
-            luc_selector.options = list(source_data["LUC"].unique())
-
-    unit_selector.param.watch(update_luc_selector, "value")
-
-    widgets = pn.Row(
-        scenario_selector, unit_selector, luc_selector, sizing_mode="scale_width"
+    fd = pn.widgets.FileDownload(  # FileDownload widget to download the filtered data
+        callback=pn.bind(
+            get_filtered_file,
+            scenario=scenario_selector,
+            luc=luc_selector,
+            unit=unit_selector,
+        ),
+        filename="data.csv",
     )
 
+    # ------------------- Update Widgets --------------
+    unit_selector.param.watch(update_luc_selector, "value")
+
+    # ------------------- Create Dashboard --------------
     dashboard = pn.Row(
         pn.Column(
             # pn.pane.Markdown("### Charging Power Trends"),
@@ -254,13 +258,59 @@ def create_scenario_graphs():
     )
 
     return dashboard
-    # pn.extension(global_css=[':root { --design-background-color: purple; }'])
-
-    # dashboard.show()
-    # dashboard.save("/Users/carlos.canales/projects/hosting/carlosrodrigo5.github.io/index.html", embed=True)
 
 
 def create_sensitivity_analysis_graph():
+    # --------------- Functions --------------
+    def filter_data_table(parameter, luc, unit):
+        data = source_data.copy()
+        data["value"] = data["value"].round(2)
+        if parameter != "Alle":
+            data = data[data["parameter"] == parameter]
+        if luc and len(luc) > 0:
+            data = data[data["LUC"] == luc]
+        data = data[data["unit"] == unit].reset_index(drop=True)
+        return data.drop(columns=["color"])
+
+    def filter_data_plot(parameter, luc, unit):
+        # if luc and len(luc) > 0:
+        #     data = data[data['LUC'] == luc]
+        # data =
+        return source_data[
+            (source_data["unit"] == unit)
+            & (source_data["LUC"] == luc)
+            & (source_data["parameter"] == parameter)
+        ].set_index(["parameter"])
+        # fig = df_plot.hvplot.scatter(
+        #     x='year', y='value', by='scenario',
+        #     title=f'Unit: {unit_selector.value} - LUC: {luc_selector.value}',
+        #     legend='top_left',
+        #     tools=[hover],
+        #     responsive=True,
+        #     muted_alpha=0,
+        #     yformatter='%.0f'
+        # ).opts(legend_opts={"click_policy": "hide"})
+    def get_filtered_file(parameter, luc, unit):
+        df = filter_data_table(parameter, luc, unit)
+        sio = StringIO()
+        df.to_csv(sio)
+        sio.seek(0)
+        return sio
+    # update luc_selector options based on unit selection
+
+    def update_luc_selector(event):
+        if event.new == "MW":
+            luc_selector.options = list(
+                source_data[source_data["unit"] == "MW"]["LUC"].unique()
+            )
+        elif event.new == "GWh/year":
+            luc_selector.options = list(
+                source_data[source_data["unit"] == "GWh/year"]["LUC"].unique()
+            )
+        else:
+            luc_selector.options = list(source_data["LUC"].unique())
+
+    # --------------- Data Preparation -----------
     reference = simbev_results.copy()
     reference = reference[
         (reference["scenario"] == "Referenzszenario") & (reference["year"] == 2030)
@@ -291,7 +341,7 @@ def create_sensitivity_analysis_graph():
         source_data["variable"] == "Referenzszenario 2030", "#e0b90b", "#0b68e0"
     )
 
-    # Widgets
+    # -------------------- Create Widgets --------------
     parameter_selector = pn.widgets.Select(
         name="Parameter \n (nur für Tabelle)",
         options=list(source_data["parameter"].unique()),
@@ -311,40 +361,14 @@ def create_sensitivity_analysis_graph():
         sizing_mode="scale_width",
     )
 
-    def filter_data_table(parameter, luc, unit):
-        data = source_data.copy()
-        data["value"] = data["value"].round(2)
-        if parameter != "Alle":
-            data = data[data["parameter"] == parameter]
-        if luc and len(luc) > 0:
-            data = data[data["LUC"] == luc]
-        data = data[data["unit"] == unit].reset_index(drop=True)
-        return data.drop(columns=["color"])
+    widgets = pn.Row(
+        parameter_selector, unit_selector, luc_selector, sizing_mode="scale_width"
+    )
 
-    def filter_data_plot(parameter, luc, unit):
-        # if luc and len(luc) > 0:
-        #     data = data[data['LUC'] == luc]
-        # data =
-        return source_data[
-            (source_data["unit"] == unit)
-            & (source_data["LUC"] == luc)
-            & (source_data["parameter"] == parameter)
-        ].set_index(["parameter"])
-        # fig = df_plot.hvplot.scatter(
-        #     x='year', y='value', by='scenario',
-        #     title=f'Unit: {unit_selector.value} - LUC: {luc_selector.value}',
-        #     legend='top_left',
-        #     tools=[hover],
-        #     responsive=True,
-        #     muted_alpha=0,
-        #     yformatter='%.0f'
-        # ).opts(legend_opts={"click_policy": "hide"})
-
-    # df for table
+    # ----------------- Data Filtering --------------
     df_table = pn.rx(filter_data_table)(
         parameter=parameter_selector, luc=luc_selector, unit=unit_selector
     )
-    # df_plot = pn.rx(filter_data_plot)(luc=luc_selector, unit=unit_selector)
     df_plot = hvplot.bind(
         filter_data_plot,
         parameter=parameter_selector,
@@ -352,28 +376,11 @@ def create_sensitivity_analysis_graph():
         unit=unit_selector,
     ).interactive()
 
-    def filtered_file(parameter, luc, unit):
-        df = filter_data_table(parameter, luc, unit)
-        sio = StringIO()
-        df.to_csv(sio)
-        sio.seek(0)
-        return sio
-
-    fd = pn.widgets.FileDownload(
-        callback=pn.bind(
-            filtered_file,
-            parameter=parameter_selector,
-            luc=luc_selector,
-            unit=unit_selector,
-        ),
-        filename="data.csv",
-    )
-
+    # ----------------- Create Plots --------------
     hover = HoverTool(
         tooltips=[
-            ("Parameter", "@parameter"),
+            ("Variable", "@variable"),
             ("Werte", "@value{0.0,0}"),
-            ("LUC", "@LUC"),
         ]
     )
 
@@ -384,7 +391,7 @@ def create_sensitivity_analysis_graph():
         ylabel="Werte",
         xlabel="Variabel",
         color="color",
-        # tools=[hover],
+        tools=[hover],
         responsive=True,
         muted_alpha=0,
     ).opts(
@@ -402,27 +409,23 @@ def create_sensitivity_analysis_graph():
         # height=400
     )
 
+    # ------------ Create Table --------------
     table = pn.widgets.Tabulator(df_table, name="table", sizing_mode="scale_both")
 
-    # update luc_selector options based on unit selection
-    def update_luc_selector(event):
-        if event.new == "MW":
-            luc_selector.options = list(
-                source_data[source_data["unit"] == "MW"]["LUC"].unique()
-            )
-        elif event.new == "GWh/year":
-            luc_selector.options = list(
-                source_data[source_data["unit"] == "GWh/year"]["LUC"].unique()
-            )
-        else:
-            luc_selector.options = list(source_data["LUC"].unique())
-
-    unit_selector.param.watch(update_luc_selector, "value")
-
-    widgets = pn.Row(
-        parameter_selector, unit_selector, luc_selector, sizing_mode="scale_width"
+    fd = pn.widgets.FileDownload( # FileDownload widget to download the filtered data
+        callback=pn.bind(
+            get_filtered_file,
+            parameter=parameter_selector,
+            luc=luc_selector,
+            unit=unit_selector,
+        ),
+        filename="data.csv",
     )
 
+    # ----------------- Update Widgets --------------
+    unit_selector.param.watch(update_luc_selector, "value")
+
+    # ----------------- Create Dashboard --------------
     dashboard = pn.Row(
         pn.Column(
             # pn.pane.Markdown("### Charging Power Trends"),
@@ -441,10 +444,6 @@ def create_sensitivity_analysis_graph():
     )
 
     return dashboard
-    # pn.extension(global_css=[':root { --design-background-color: purple; }'])
-
-    # dashboard.show()
-    # dashboard.save("/Users/carlos.canales/projects/hosting/carlosrodrigo5.github.io/index.html", embed=True)
 
 
 ############################# WIDGETS & CALLBACK ###########################################
@@ -489,24 +488,8 @@ def create_page2():
     main_area.append(dashboard2)
     # return obj
 
-
-# mapping = {
-#     "Page1": create_page1(),
-#     "Page2": create_page2(),
-# }
-
-
-# def show_page(page_key):
-#     main_area.clear()
-#     main_area.append(mapping[page_key])
-
-
 button1.on_click(lambda event: create_page1())
 button2.on_click(lambda event: create_page2())
-
-#
-# button1.on_click(lambda event: (main_area.clear(), main_area.append(mapping["Page1"])))
-# button2.on_click(lambda event: (main_area.clear(), main_area.append(mapping["Page2"])))
 
 button1.js_on_click(
     args={},
@@ -514,12 +497,6 @@ button1.js_on_click(
     window.location.reload();
 """,
 )
-# button2.js_on_click(
-#     args={},
-#     code="""
-#     window.location.reload();
-# """,
-# )
 
 #################### SIDEBAR LAYOUT ##########################
 sidebar = pn.Column(
@@ -528,47 +505,6 @@ sidebar = pn.Column(
     button2,
     styles={"width": "100%", "padding": "15px"},
 )
-
-# starting_page = pn.state.session_args.get("page", [b"Page2"])[0].decode()
-# sidebar = pn.widgets.RadioButtonGroup(
-#     value=starting_page,
-#     options=list(mapping.keys()),
-#     name="Page",
-#     sizing_mode="fixed",
-#     button_type="success",
-# )
-
-
-# def show(page):
-#     return mapping[page]
-#
-#
-# main_area = pn.bind(show, page=sidebar)
-# sidebar.param.watch(show, 'value')
-# pn.state.location.sync(sidebar, {'value': 'page'})
-
-
-#################### MAIN LAYOUT ##########################
-# main = pn.Column(
-#     pn.pane.Markdown("## Dashboard"),
-#     create_page1(),
-#     sizing_mode="stretch_width",
-# )
-
-################### MAIN AREA LAYOUT ##########################
-
-
-#################### TEMPLATE LAYOUT ##########################
-# template = pn.template.FastListTemplate(
-#     title="SimBeV Dashboard",
-#     sidebar=sidebar,
-#     main=[main_area],
-#     theme=pn.template.theme,
-#     header_background="#f0f0f0",
-#     header_color="#000000",
-#     header_font_size="20px",
-#     sidebar_width=200,
-# )
 
 template = pn.template.FastListTemplate(
     title="SimBEV Ergebnisse",
@@ -581,12 +517,4 @@ template = pn.template.FastListTemplate(
 )
 
 template.servable()
-# template.show(
-#     title="SimBEV Dashboard",
-#     location="http://localhost:5006",
-#     # location="https://simbev-dashboard.carlosrodrigo5.workers.dev/",
-#     # location="https://simbev-dashboard.carlosrodrigo5.workers.dev/",
-#     # location="https://simbev-dashboard.carlosrodrigo5.workers.dev/",
-# )
-# template.show()
-# template.show()
+#template.show()
